18c41ea67d75f4d43cc94563bd42846b04c3d234
ashusc <ashutosh.mishra@socialcodeinc.com>
2018-07-01 17:39:20 +0530

    Xpath adding

--- src/test/java/org/nng/qa/framework/BDDScreenplay/ui/Samples.java
+++ src/test/java/org/nng/qa/framework/BDDScreenplay/ui/Samples.java
@@ -1,5 +1,13 @@
+// PACKAGE
 package org.nng.qa.framework.BDDScreenplay.ui;
 
+// IMPORT
+import net.serenitybdd.screenplay.targets.Target;
+
+// CLASS
 public class Samples {
 
+		// XPATH SAMPLES WITH AXIS
+		public static Target SAMPLE_WITH_PROPERTY_CAONTAINS = Target.the("Element that contains some property").locatedBy("//*[contains(@class, 'primary-header')]");
+		
 }
