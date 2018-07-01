6dbf0f91db4c48794ae7ad15d2cb352369142bde
ashusc <ashutosh.mishra@socialcodeinc.com>
2018-07-01 17:00:13 +0530

    Chages for actions and question class

---
+++ src/test/java/org/nng/qa/framework/BDDScreenplay/actions/DeleteBrowserStorage.java
@@ -0,0 +1,56 @@
+// Package
+package org.nng.qa.framework.BDDScreenplay.actions;
+
+// Import Section
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+import org.nng.qa.framework.BDDScreenplay.commons.Constants;
+import org.openqa.selenium.JavascriptExecutor;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Interaction;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.thucydides.core.annotations.Screenshots;
+import net.thucydides.core.annotations.Step;
+
+// Class
+public class DeleteBrowserStorage implements Interaction {
+	//---------------------------------------------------------
+	// GLOBALs
+	//---------------------------------------------------------	
+	private String storageVarNameToBeDeleted;
+	//---------------------------------------------------------
+	// Constructors
+	//---------------------------------------------------------		
+	public DeleteBrowserStorage() {}
+	public DeleteBrowserStorage(String storageVarName) { this.storageVarNameToBeDeleted = storageVarName; }
+	
+	//---------------------------------------------------------
+	// GLOBALs
+	//---------------------------------------------------------	
+	@Override
+	@Step("{0} deletes the browser storage")
+	@Screenshots(disabled = true)
+	public <T extends Actor> void performAs(T actor) {
+		// Wait for the Element, Based on Timeout
+		if(this.storageVarNameToBeDeleted != Constants.NULL) {
+			// Delete specific storage
+			
+		} else {
+			// Delete all storage
+			// Initialize JSExecutor
+			JavascriptExecutor js = (JavascriptExecutor) BrowseTheWeb.as(actor).getDriver();
+			// clear local session storage
+			js.executeScript(String.format("window.localStorage.clear();"));
+		}
+	}
+
+	//---------------------------------------------------------
+	// Action Builders
+	//---------------------------------------------------------		
+	public static DeleteBrowserStorage all() {
+		return instrumented(DeleteBrowserStorage.class);
+	}
+	public static DeleteBrowserStorage byVariableName(String storageVariableName) {
+		return instrumented(DeleteBrowserStorage.class, storageVariableName);
+	}
+}
