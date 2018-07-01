77cb8f4c1c3af45feb1450a6f4c35dedbe7bf668
ashusc <ashutosh.mishra@socialcodeinc.com>
2018-07-01 17:43:18 +0530

    Some XPath pattern added in UI Sample to help create target elements

--- src/test/java/org/nng/qa/framework/BDDScreenplay/ui/Samples.java
+++ src/test/java/org/nng/qa/framework/BDDScreenplay/ui/Samples.java
@@ -7,6 +7,11 @@ import net.serenitybdd.screenplay.targets.Target;
 // CLASS
 public class Samples {
 
+		// XPath Axis to be used further
+		// For Next Sibling:: //*/following-sibling::<Conditions>
+		// For Childs:: //*/descendant::<Conditions>
+		// For Parent:: //*/ancestor::<Conditions>
+		
 		// XPATH SAMPLES WITH AXIS
 		public static Target SAMPLE_WITH_PROPERTY_CAONTAINS = Target.the("Element that contains some property").locatedBy("//*[contains(@class, 'primary-header')]");
 		
