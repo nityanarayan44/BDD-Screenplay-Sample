ce1b5cce29dab6976b2b6533fdbc4225214e1164
Pradeep Yadav <>
2018-07-04 22:00:52 +0530

    yahoo send email verify email

---
+++ src/test/java/org/nng/qa/framework/BDDScreenplay/tasks/yahoo/ComposeEmail.java
@@ -0,0 +1,41 @@
+package org.nng.qa.framework.BDDScreenplay.tasks.yahoo;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
+import org.nng.qa.framework.BDDScreenplay.ui.yahoo.Yahoo;
+
+import net.serenitybdd.core.steps.Instrumented;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+
+public class ComposeEmail implements Task {
+
+	private String email;
+	private String subject;
+	private String body;
+	
+	public ComposeEmail(String email, String subject, String body) {
+		this.email = email;
+		this.subject = subject;
+		this.body = body;
+	}
+
+	@Override
+	public <T extends Actor> void performAs(T actor) {
+		// TODO Auto-generated method stub
+		actor.attemptsTo(Click.on(Yahoo.COMPOSE_BTN));
+		actor.attemptsTo(Click.on(Yahoo.RECIPIENT), Enter.theValue(email).into(Yahoo.RECIPIENT));
+		actor.attemptsTo(Enter.theValue(subject).into(Yahoo.SUBJECT));
+		actor.attemptsTo(Enter.theValue(body).into(Yahoo.MESSAGE_BODY));
+		actor.attemptsTo(Click.on(Yahoo.SEND));
+
+	}
+
+	
+
+	public static ComposeEmail sendEmail(String email, String subject, String body) {
+		return Instrumented.instanceOf(ComposeEmail.class).withProperties(email, subject, body);
+	}
+
+}
